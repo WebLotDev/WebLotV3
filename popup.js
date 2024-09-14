6316a4c3148fa73c97b44c0fbee7b2c6e22611e5
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-09-14 03:12:50 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,89 +1,29 @@
-// Get the popup elements
-const popupOverlay = document.getElementById("popupOverlay");
-const openPopupBtn = document.getElementById("openPopupBtn");
-const closePopupBtn = document.querySelector(".closePopup");
+// General function to manage popups
+function handlePopup(openBtnId, popupOverlayId, closeBtnClass) {
+  const popupOverlay = document.getElementById(popupOverlayId);
+  const openPopupBtn = document.getElementById(openBtnId);
+  const closePopupBtn = popupOverlay.querySelector(closeBtnClass);
 
-// Open the popup
-openPopupBtn.addEventListener("click", () => {
-  popupOverlay.style.display = "block";
-});
+  // Open the popup
+  openPopupBtn.addEventListener("click", () => {
+    popupOverlay.style.display = "block";
+  });
 
-// Close the popup
-closePopupBtn.addEventListener("click", () => {
-  popupOverlay.style.display = "none";
-});
-
-// Close the popup when clicking outside of the popup content
-window.addEventListener("click", (event) => {
-  if (event.target == popupOverlay) {
+  // Close the popup
+  closePopupBtn.addEventListener("click", () => {
     popupOverlay.style.display = "none";
-  }
-});
+  });
 
-// Get the popup elements
-const popupOverlay2 = document.getElementById("popupOverlay2");
-const openPopupBtn2 = document.getElementById("openPopupBtn2");
-const closePopupBtn2 = document.querySelector(".closePopup2");
+  // Close the popup when clicking outside of the popup content
+  window.addEventListener("click", (event) => {
+    if (event.target == popupOverlay) {
+      popupOverlay.style.display = "none";
+    }
+  });
+}
 
-// Open the popup
-openPopupBtn2.addEventListener("click", () => {
-  popupOverlay2.style.display = "block";
-});
-
-// Close the popup
-closePopupBtn2.addEventListener("click", () => {
-  popupOverlay2.style.display = "none";
-});
-
-// Close the popup when clicking outside of the popup content
-window.addEventListener("click", (event) => {
-  if (event.target == popupOverlay2) {
-    popupOverlay2.style.display = "none";
-  }
-});
-
-
-// Get the popup elements
-const popupOverlay3 = document.getElementById("popupOverlay3");
-const openPopupBtn3 = document.getElementById("openPopupBtn3");
-const closePopupBtn3 = document.querySelector(".closePopup3");
-
-// Open the popup
-openPopupBtn3.addEventListener("click", () => {
-  popupOverlay3.style.display = "block";
-});
-
-// Close the popup
-closePopupBtn3.addEventListener("click", () => {
-  popupOverlay3.style.display = "none";
-});
-
-// Close the popup when clicking outside of the popup content
-window.addEventListener("click", (event) => {
-  if (event.target == popupOverlay3) {
-    popupOverlay3.style.display = "none";
-  }
-});
-
-
-// Get the popup elements
-const popupOverlay4 = document.getElementById("popupOverlay4");
-const openPopupBtn4 = document.getElementById("openPopupBtn4");
-const closePopupBtn4 = document.querySelector(".closePopup4");
-
-// Open the popup
-openPopupBtn4.addEventListener("click", () => {
-  popupOverlay4.style.display = "block";
-});
-
-// Close the popup
-closePopupBtn4.addEventListener("click", () => {
-  popupOverlay4.style.display = "none";
-});
-
-// Close the popup when clicking outside of the popup content
-window.addEventListener("click", (event) => {
-  if (event.target == popupOverlay4) {
-    popupOverlay4.style.display = "none";
-  }
-});
+// Manage all popups
+handlePopup("openPopupBtn", "popupOverlay", ".closePopup");
+handlePopup("openPopupBtn2", "popupOverlay2", ".closePopup2");
+handlePopup("openPopupBtn3", "popupOverlay3", ".closePopup3");
+handlePopup("openPopupBtn4", "popupOverlay4", ".closePopup4");
--- script.js
+++ script.js
@@ -13,5 +13,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 
+// Get the sidebar menu and checkbox
+const sidebarMenu = document.getElementById('sidebarMenu');
+const sidebarCheckbox = document.getElementById('openSidebarMenu');
+
+// Get all links inside the sidebar menu
+const sidebarLinks = document.querySelectorAll('#sidebarMenu .sidebarMenuInner li a');
+
+// Add event listeners to each link
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Uncheck the checkbox to close the sidebar menu
+        sidebarCheckbox.checked = false;
+    });
+});
 
   
