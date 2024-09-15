5dd5f638ed2380ea907dea220126e7c0aeb8c8bb
George Antonakos <antonakosgeorge7@gmail.com>
2024-09-15 23:30:57 +0300

    buttons

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-// Get the sidebar menu and checkbox
 const sidebarMenu = document.getElementById('sidebarMenu');
 const sidebarCheckbox = document.getElementById('openSidebarMenu');
 
-// Get all links inside the sidebar menu
 const sidebarLinks = document.querySelectorAll('#sidebarMenu .sidebarMenuInner li a');
 
-// Add event listeners to each link
 sidebarLinks.forEach(link => {
     link.addEventListener('click', () => {
-        // Uncheck the checkbox to close the sidebar menu
         sidebarCheckbox.checked = false;
     });
 });
--- popup.js
+++ popup.js
@@ -1,20 +1,16 @@
-// General function to manage popups
 function handlePopup(openBtnId, popupOverlayId, closeBtnClass) {
   const popupOverlay = document.getElementById(popupOverlayId);
   const openPopupBtn = document.getElementById(openBtnId);
   const closePopupBtn = popupOverlay.querySelector(closeBtnClass);
 
-  // Open the popup
   openPopupBtn.addEventListener("click", () => {
     popupOverlay.style.display = "block";
   });
 
-  // Close the popup
   closePopupBtn.addEventListener("click", () => {
     popupOverlay.style.display = "none";
   });
 
-  // Close the popup when clicking outside of the popup content
   window.addEventListener("click", (event) => {
     if (event.target == popupOverlay) {
       popupOverlay.style.display = "none";
@@ -22,7 +18,6 @@ function handlePopup(openBtnId, popupOverlayId, closeBtnClass) {
   });
 }
 
-// Manage all popups
 handlePopup("openPopupBtn", "popupOverlay", ".closePopup");
 handlePopup("openPopupBtn2", "popupOverlay2", ".closePopup2");
 handlePopup("openPopupBtn3", "popupOverlay3", ".closePopup3");
